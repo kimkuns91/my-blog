1560064720874cd983422f2c2f96833f2d8705ad
Kun Woo Kim <kimkuns98@gmail.com>
2024-02-21 17:50:14 +0900

    에러 체크

--- src/app/api/auth/userinfo/route.ts
+++ src/app/api/auth/userinfo/route.ts
@@ -1,41 +1,38 @@
-import { authOptions } from '@/libs/next-auth';
-import prisma from '@/libs/prisma';
-import { getServerSession } from 'next-auth';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
   try {
-    const session = await getServerSession(authOptions);
+    // const session = await getServerSession(authOptions);
 
-    if (!session) {
-      return NextResponse.json(
-        {
-          message: '세션 정보가 만료되었습니다. 다시 로그인 해주세요.',
-        },
-        {
-          status: 409,
-        }
-      );
-    }
-    const userId = session.user.id;
-    const user = await prisma.user.findUnique({
-      where: {
-        id: userId,
-      },
-    });
+    // if (!session) {
+    //   return NextResponse.json(
+    //     {
+    //       message: '세션 정보가 만료되었습니다. 다시 로그인 해주세요.',
+    //     },
+    //     {
+    //       status: 409,
+    //     }
+    //   );
+    // }
+    // const userId = session.user.id;
+    // const user = await prisma.user.findUnique({
+    //   where: {
+    //     id: userId,
+    //   },
+    // });
 
-    if (!user) {
-      return NextResponse.json(
-        {
-          message: '해당 이메일로 가입된 유저가 없습니다.',
-        },
-        {
-          status: 409,
-        }
-      );
-    }
+    // if (!user) {
+    //   return NextResponse.json(
+    //     {
+    //       message: '해당 이메일로 가입된 유저가 없습니다.',
+    //     },
+    //     {
+    //       status: 409,
+    //     }
+    //   );
+    // }
 
-    return NextResponse.json(user, { status: 200 });
+    return NextResponse.json({ user: '' }, { status: 200 });
   } catch (error) {
     return NextResponse.error();
   }
