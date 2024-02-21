8cea96922dcd16692811ac4e155db8de16b63157
Kun Woo Kim <kimkuns98@gmail.com>
2024-02-21 17:46:39 +0900

    Try

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-const colors = require("tailwindcss/colors");
+// const defaultTheme = require("tailwindcss/defaultTheme");
+// const colors = require("tailwindcss/colors");
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
